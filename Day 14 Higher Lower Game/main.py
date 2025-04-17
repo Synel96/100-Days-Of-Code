baa8f3ae580c7488464055a6973f647194c3b0be
Szilveszter Nemeth <nmszilveszter@gmail.com>
2025-04-17 17:55:36 +0200

    Add files via upload

---
+++ Day 14 Higher Lower Game/main.py
@@ -0,0 +1,41 @@
+import random 
+import game_data
+import art
+
+def call_new_person():
+    return random.choice(game_data.data)
+
+a_option = call_new_person()
+b_option = call_new_person()
+player_score = 0
+
+
+def compare_followers(followers_1, followers_2):
+    if followers_1["follower_count"] > followers_2["follower_count"] :
+        return followers_1["name"]
+    else:
+        return followers_2
+    
+right_answer = compare_followers(a_option,b_option)
+
+print(art.logo)
+correct_answer = True
+while correct_answer :
+    print(f"Compare A :{a_option["name"]}, {a_option["description"]} from {a_option["country"]} ")
+    print(art.vs)
+    print(f"Againts B : {b_option["name"]}, {b_option["description"]} from {b_option["country"]}")
+    player_choice = input("Who has more followers? Type 'A' or 'B' : ").lower()
+    if player_choice == "a" and right_answer == a_option:
+        player_score += 1
+        print(f"Correct!Your Score : {player_score}")
+        a_option = right_answer
+        b_option = call_new_person()
+    elif player_choice == "b" and right_answer == b_option:
+        player_score += 1
+        print(f"Correct!Your Score : {player_score}")
+        a_option = right_answer
+        b_option = call_new_person()
+    else:
+        print(f"False! You Lose!Your Final Score is : {player_score}")
+        correct_answer = False
+    
---
+++ Day 14 Higher Lower Game/todo.py
@@ -0,0 +1,7 @@
+# the game needs to pick two random key from the game_data and save them(importing random module and game_data , create two variable to store the keys )
+# we need a function to compare them.(a_option  > b_option? ) 
+# game inputs 
+# if the player chose false : game over , else if : the right choice should be = a_option .
+# need a loop to start again if player choice was right
+# game over outputs (win / lose print the correct answers)
+# add game scores and printing it to the console each turn
