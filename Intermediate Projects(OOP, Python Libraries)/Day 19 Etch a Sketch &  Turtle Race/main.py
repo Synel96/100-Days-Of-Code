b170afd2e901042840131400c2f104c3499b0b6c
Szilveszter Nemeth <nmszilveszter@gmail.com>
2025-05-06 09:54:52 +0200

    Finished day 19

--- Intermediate Projects(OOP, Python Libraries)/Day 19 Etch a Sketch &  Turtle Race/main.py
+++ Intermediate Projects(OOP, Python Libraries)/Day 19 Etch a Sketch &  Turtle Race/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 
 screen = Screen()
@@ -6,16 +7,39 @@ screen.setup(width=500,height=400)
 user_bet = screen.textinput(title="Make your bet",prompt="Wich turtle will win the race? Enter a color : ")
 
 
-is_race_one = False
+is_race_on= False
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
-for turtle_index in range(0,6):
-    tim = Turtle("turtle")
-    tim.penup()
-    tim.goto(x=-230,y=y_positions[turtle_index])
-    tim.color(colors[turtle_index])
+all_turtles = []
 
-while 
+
+for turtle_index in range(0,6):
+    new_turtle = Turtle("turtle")
+    new_turtle.penup()
+    new_turtle.goto(x=-230,y=y_positions[turtle_index])
+    new_turtle.color(colors[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet :
+    is_race_on = True
+
+
+while is_race_on:
+
+    
+
+    for turtle in all_turtles:
+        if turtle.xcord() > 230:
+            winning_color = turtle.pencolor()
+            is_race_on = False
+        if winning_color == user_bet:
+            print(f"You've won ! The {winning_color} color turtle is the winner!")
+            
+        else:
+            print((f"You've lost ! The {winning_color} color turtle is the winner!"))
+        random_distance = random.randint(0,10)
+        turtle.forward(random_distance)
+
 
     
 
